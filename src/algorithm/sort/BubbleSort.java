9be1074e80ab9c6638067816ef9750cfdac318e4
WJH <7931946+jianhao1997@user.noreply.gitee.com>
2024-07-24 00:25:40 +0800

    冒泡排序优化，只要不再发生交换，可认为后续已经是有序的了

--- src/algorithm/sort/BubbleSort.java
+++ src/algorithm/sort/BubbleSort.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr = {324, 4, 54, 6, -1, -34, 87, 46, 234, 23, 56, 675, 658, 76, 232};
+        int[] arr = {324, 4, 54, 6, -1, -34, 87, 46, 234, 23, 56, 675, 658, 76, 232, 456, 567, 678, 789, 890, 1000};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
@@ -21,13 +21,19 @@ public class BubbleSort {
         }
         int len = arr.length;
         for (int i = 0; i < len; i++) {
+            boolean flag = false;
             for (int j = 1; j < len - i; j++) {
                 if (arr[j] < arr[j - 1]) { // 相等不交换，稳定排序
                     // 交换位置
                     int tem = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = tem;
+                    flag = true; // 发生交换
                 }
+            }
+            if (!flag) {
+                // 此轮没有发生交换，后面已经是有序的了
+                break;
             }
         }
     }
